eeeb2ce5eec1302557e3e7d16b6ae2b931081ff5
chordsey <alexmart.heredia@gmail.com>
2021-04-21 09:34:40 -0700

    feat: begins working on form for changing field properties by changing the values programatically

--- src/components/InfoModal/InfoModal.js
+++ src/components/InfoModal/InfoModal.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import { Rnd } from 'react-rnd';
+import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles({
   draggable: {
@@ -17,13 +18,14 @@ const useStyles = makeStyles({
     flexGrow: 1,
     backgroundColor: 'rgba(211, 211, 211, 0.95)',
     position: 'sticky',
-    bottom: '10px'
+    bottom: '10px',
+    padding: '10px'
   },
   button: {
     margin: '5px'
   },
   jsonObj: {
-    fontSize: 12,
+    fontSize: 20,
     height: '225px',
     overflow: 'auto'
   },
@@ -43,9 +45,14 @@ const useStyles = makeStyles({
 function InfoModal({setSelectedField, selectedField, fields, setFields, options, setOptions}) {
   const styles = useStyles();
   const [selectedOption, setSelectedOption] = useState()
+  const { register, handleSubmit, setValue } = useForm(); 
 
+  const applyChanges = changes => {
+    if (changes.name === selectedField.name) {
+      const updatedField = {...selectedField, ...changes}
+    }
+  }
   const selectOption = option => {
-    console.log(option)
     setSelectedOption(option)
   }
 
@@ -60,9 +67,15 @@ function InfoModal({setSelectedField, selectedField, fields, setFields, options,
   }
 
   useEffect(() => {
-    console.log(options)
-  }, [fields, options])
+    console.log(selectedField?.width)
+    Object.keys(selectedField || {}).map(key => {
+      if (['x', 'y', 'width', 'height', 'name'].indexOf(key) > -1) {
+        setValue(`${key}`, selectedField[key])
+      }
+    })
+  }, [fields, options, selectedField, selectedField?.height])
 
+  if (!Object.keys(fields).length) return null
   return (
     <Rnd
       style={{ zIndex: 1001 }}
@@ -72,8 +85,37 @@ function InfoModal({setSelectedField, selectedField, fields, setFields, options,
         <div>
           {selectedField && (
             <div className={styles.container}>
-              <pre className={styles.jsonObj}>{JSON.stringify(selectedField, null, 4)}</pre>
-              
+              <form onSubmit={handleSubmit(applyChanges)}>
+                <div className={styles.container}>
+
+                <pre className={styles.jsonObj}>{JSON.stringify(selectedField, null, 4)}</pre>
+                {/* {Object.keys(selectedField).filter(key => key !== 'options' && key !== 'type').map(key => (
+                  <div>
+                      <p>{key}:</p>
+                      <input placeholder={selectedField[key]} defaultValue={selectedField[key]} name={key} ref={register}/>
+                  </div>
+                ))} */}
+                {/* {selectedField.type === 'select' && (
+                  <select name='type'>
+                    <options value='text'>text</options>
+                    <options value='select'>select</options>
+                    <options value='checkbox'>checkbox</options>
+                  </select>
+                )}
+                {selectedField.type ='select' && selectedField.options.map(option => {
+                  <div className={styles.container}>
+                    <p>Options</p>
+                    <div>
+                      <p>label:</p>
+                      <input defaultValue={option.label}/>
+                      <p>value:</p>
+                      <input defaultValue={option.value}/>
+                    </div>
+                  </div>
+                }) } */}
+              </div>
+                <button type='submit' className={styles.button}>apply changes</button>
+              </form>
               <button className={styles.button} onClick={() => deleteField(selectedField)}>delete</button>
             </div>
           )}
